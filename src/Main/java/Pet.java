2a88d011c7872e1179432d884b6e7f32ad11ad2c
Flexxelexx <alexxkooij@hotmail.com>
2022-06-20 15:15:02 +0200

    up to setSiblings

--- src/Main/java/Pet.java
+++ src/Main/java/Pet.java
@@ -1,2 +1,52 @@
-package PACKAGE_NAME;public class Pet {
+import java.awt.*;
+import java.util.List;
+
+public class Pet {
+
+    private String name;
+    private int age;
+    private String species;
+    private List <Person> owners;
+
+    public Pet(String name, int age, String species) {
+        this.name = name;
+        this.age = age;
+        this.species = species;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getSpecies() {
+        return species;
+    }
+
+    public void setSpecies(String species) {
+        this.species = species;
+    }
+
+    public List<Person> getOwner() {
+        return owners;
+    }
+
+    public void setOwner (List<Person> owner) {
+        this.owners = owner;
+    }
+
+public void addOwner (Person owner) {
+        owners.add(owner);
+    }
 }
